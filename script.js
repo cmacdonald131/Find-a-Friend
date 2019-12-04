2765731c4c46e014997a8a53feccf689d5696cb4
Christopher MacDonald <55043368+cmacdonald131@users.noreply.github.com>
2019-12-04 15:08:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function() {
+    $('input[type="text"]').focus();
+
+    var inputPlaceholder = ["Dog", "Cat", "Bird", "Snake", "Turtle", "Hamster"];
+    setInterval(function() {
+        $("input[type='text']").attr("placeholder", inputPlaceholder[inputPlaceholder.push(inputPlaceholder.shift())-1]);
+    }, 3000);
+})
+
+var key="OsktMS2zlvj2EkLt90UkTiTgUJdAdbO6SZvSHxW1hDHQp2er1t";
+var secret="MKsIqvvUXPSACsQhlB8RPh0t9BqRVa3XVdsTa6Wq";
+
+var type= $('#petSearch').val();
+
+fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method: "POST",
+    body: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+}).then(function(resp) {
+    return resp.json();
+}).then(function(data) {
+    console.log('token', data);
+
+return fetch("https://api.petfinder.com/v2/types/?type=" + type, {
+     headers: {
+        'Authorization': data.token_type + ' ' + data.access_token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+}).then(function(resp) {
+    return resp.json();
+
+}).then(response => {
+    console.log(response);
+    let buildHTML = "";
+    for (let i = 0; i < response.data; i++) {
+        buildHTML += `<h2>${response.data[i].fullName}</h2>`;
+        buildHTML += `<p><a href="${response.data[i].url}" target="_blank">Visit Website</a></p>`;
+        buildHTML += `<p><em>${response.data[i].description}</em></p>`;
+    }
+    document.querySelector("#rescue").innerHTML = buildHTML;
+});
+
+document.querySelector("#submit").addEventListener("click", (e) => {
+    e.preventDefault();
+})
