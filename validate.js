a02b9d75d8d1064b018a12c1fcc58273bc85f4ae
Christopher MacDonald <55043368+cmacdonald131@users.noreply.github.com>
2019-12-23 23:56:45 -0500

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,13 +1,20 @@
 var key="OsktMS2zlvj2EkLt90UkTiTgUJdAdbO6SZvSHxW1hDHQp2er1t";
 var secret="MKsIqvvUXPSACsQhlB8RPh0t9BqRVa3XVdsTa6Wq";
 
-fetch('https://api.petfinder.com/v2/oauth2/token', {
-    method: "POST",
-    body: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
-       headers: {
+let token=""
+
+function getToken() {
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: "POST",
+        body: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
+         headers: {
            "Content-Type": "application/x-www-form-urlencoded"
+           
         }
-}).then(function(resp) {
+    }).then(function(resp) {
     return resp.json();
-}).then(function(data) {
+    }).then(function(data) {
+    token= data
     console.log('token', data)})
+}
+getToken();
