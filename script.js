7a044140ebc55bda267ded89640d0b2e2c65d321
Christopher MacDonald <55043368+cmacdonald131@users.noreply.github.com>
2019-12-23 23:53:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 'use strict';
 
-
-let animal = $('#petSearch').val();
-
-let zip = $('#zip').val();
-
-
-
-function fetchAnimals() {
-    fetch(`https://api.petfinder.com/v2/animals?type=${animal}&location=${zip}&page=2`, {headers: {
-      Accept: 'application/json'},
-        credentials: 'same-origin',
-        Authorization: `Token ${token}`})
+function fetchAnimals(animal, zip, gender) {
+  console.log(token);
+  fetch(`https://api.petfinder.com/v2/animals?type=${animal}&location=${zip}&gender=${gender}&status=adoptable&distance=25`, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`
+    }
+  })
     .then(response => response.json())
     .then(responseJson => showAnimals(responseJson))
-    .catch(error => alert("It's not working"));
+    .catch(error => showError(error.message));
 }
 
 function showAnimals(responseJson) {
   console.log(responseJson);
+  if(!responseJson.animals) {
+    showError("Something went wrong with your search.  Please try again.");
+    return
+  }
+  if(responseJson.animals.length == 0) {
+    showError("Sorry, there were no results for your search.  Please try again.");
+    return
+  }
 
-  $('#results').html("<h2>Let's check out your new pet!</h2>");
-  responseJson.message.forEach(renderedData => {
-    $('#results').append(
-      `<div class="row">
-        <h4>${type}</h4>
-        <p>${breeds}</p>
-        <p>${url}</p>`)
-  });
+  $('#results ul').html('');
+  for(let i=0; i<responseJson.animals.length; i++) {
+     $('#results ul').append(`<li>
+     <h3>${responseJson.animals[i].name}</h3>
+     
+     <p>${responseJson.animals[i].breeds.primary} (${responseJson.animals[i].age}) (${responseJson.animals[i].size})</p>
+     <a href="${responseJson.animals[i].url}" class="animalLink" target="_blank">Check Me Out</a>
+     </li>`);
+ }  
+
 }
+
+function showError(message) {
+  $('#results ul').html('');
+  $('#results ul').append(`<li>${message}</li>`);
+  console.log(message);
+}
+
+
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    fetchAnimals(animal, zip);
-    });
+    const animal = $('#petSearch').val();
+    const zip = $('#zip').val();
+    const gender = $('#gender').val();
+    fetchAnimals(animal, zip, gender);
+  });
 }
 
-$(function() {
+$(function () {
   console.log('Good to go!  Waiting to see some pets!');
   watchForm();
-  })
+})
